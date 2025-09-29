9e41eb20cff1d41cc6311477b8c06efa808a1c72
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-29 14:05:05 +0000

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hệ Thống Voucher Blockchain</h1>
+      <p>Chào mừng bạn đến với nền tảng quản lý voucher của chúng tôi!</p>
+    </div>
+  );
+}
+
+export default App;
